218900b5d0527a4007ae06a7b6d9518343288b30
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 09:39:07 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,32 @@
-console.log(4);
+class StringBuilder {
+  constructor(string) {
+    this._string = string;
+  }
+
+  get string() {
+    return this._string;
+  }
+
+  append(value) {
+    this._string = `${this._string}${value}`;
+  }
+
+  prepend(value) {
+    this._string = `${value}${this._string}`;
+  }
+
+  pad(value) {
+    this._string = `${value}${this._string}${value}`;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.string);
+
+builder.prepend('^');
+console.log(builder.string);
+
+builder.pad('=');
+console.log(builder.string);
