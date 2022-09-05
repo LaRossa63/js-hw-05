42ff830d8b231e6ce406374c3dca293482e4dad6
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 09:36:21 +0400

    Задание 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,32 @@
-console.log(3);
+class Storage {
+  constructor(abilities) {
+    this._abilities = abilities;
+  }
+
+  get abilities() {
+    return this._abilities;
+  }
+
+  addItem(item) {
+    this._abilities.push(item);
+  }
+
+  removeItem(item) {
+    this._abilities = this._abilities.filter((element) => element !== item);
+  }
+}
+
+const storage = new Storage([
+  'Супер костюм',
+  'Пролонгированного',
+  'Железные удар',
+  'Антигравитация',
+]);
+
+console.log(storage.abilities);
+
+storage.addItem('Супер сила');
+console.log(storage.abilities);
+
+storage.removeItem('Железные удар');
+console.log(storage.abilities);
